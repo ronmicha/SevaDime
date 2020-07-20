0efa4b4f689ff18a7e2e7cee284bb94c6d19a927
Ron Michaeli <ronmicha@post.bgu.ac.il>
2020-07-20 03:28:33 +0300

    removed unnecessary imports

--- src/SevaDime/pages/Tab2.tsx
+++ src/SevaDime/pages/Tab2.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import "./Tab2.css";
 
 const Tab2: React.FC = () => {
    return (
--- src/SevaDime/pages/Tab3.tsx
+++ src/SevaDime/pages/Tab3.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import "./Tab3.css";
 
 const Tab3: React.FC = () => {
    return (
